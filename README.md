dcbd0e4d9bf4a25cbf2f7b07b5b8bc3fd7249536
hudsongeovane <hudsongeovane@gmail.com>
2018-04-26 20:07:20 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+#Wappo Game
+
+Wappo is a famous puzzle game. There is a 7x7 board with monsters and one gate (to the victory).
+The wappo character moves one square in each turn, and the monsters move two squares, in order to get closer to wappo. If a monster can get wappo, he loses the game.
+
+Here is a map example.
+
+<img src="https://github.com/hudsongeovane/wappo/blob/master/public/img/game.png">
+
+This project aims to create a prototype of wappo game, with only one map. However the prototype will load maps dynamically, so if you want to contribute with this project without coding, thinking in <strike>hard</strike> maps will be a really good help.
+
+The game will be developed in javascript, using MEAN Stack.
+
+## Keywords
+
+Wappo Game, MEAN Stack, Javascript, Canvas, HTML5
---
+++ js/wappogame.js
@@ -0,0 +1,101 @@
+/*
+TREE 1
+LEFT WALL 2
+RIGHT WALL 4
+TOP WALL 8
+BOTTOM WALL 16
+PORTAL 32
+TRAP 64
+WINGAME 128
+*/
+
+var firstMap = [
+    0,0,0,0,0,64,0,
+    0,16,0,0,4,22,2,
+    0,8,0,4,6,10,0,
+    68,38,2,0,16,4,2,
+    16,16,0,0,8,16,0,
+    140,10,4,6,2,56,0,
+    0,0,0,0,0,8,0,
+    //starting position:
+    5,6,
+    //monsters position
+    1,1,
+    1,6
+    //or more...
+];
+
+var myMap = new map(firstMap);
+
+
+window.addEventListener('keydown',this.check,false);
+
+function check(e) {
+    if (myMap.GAMESTATE != 2) return;
+    
+    var code = e.keyCode;
+    console.log(e.keyCode);
+    
+    //Left arrow
+    if (code == 37) {
+	myMap.side = 3;
+	myMap.move(3);
+    }
+    //Up arrow
+    if (code == 38) {
+	myMap.side = 0;
+	myMap.move(0);
+    }
+    //Right arrow
+    if (code == 39) {
+	myMap.side = 1;
+	myMap.move(1);
+    }
+    //Down arrow
+    if (code == 40) {
+	myMap.side = 2;
+	myMap.move(2);
+    }
+    
+    myMap.checkStatus();
+}
+
+function startGame() {
+    myGameArea.start();
+}
+
+var myGameArea = {
+	canvas : document.createElement("canvas"),
+	start : function() {
+		this.canvas.width = 52*7;
+		this.canvas.height = 52*7;
+		this.context = this.canvas.getContext("2d");
+
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
+		this.frameNo = 0;
+		
+		this.interval = setInterval(updateGameArea, 100);
+	},
+	clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
+function updateGameArea() {
+        
+    
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    ctx = myGameArea.context;
+    
+    //draw map
+    myMap.draw(ctx);
+}
+
+
