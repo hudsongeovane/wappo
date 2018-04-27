5375003e73abcaef611cd05c7a98ca7ae11740d9
hudsongeovane <hudsongeovane@gmail.com>
2018-04-26 22:10:57 -0300

    Wappo almost implemented. We need to implement two-monster rule and the DEATH status.

--- js/map.js
+++ js/map.js
@@ -4,6 +4,9 @@ LEFT WALL 2
 RIGHT WALL 4
 TOP WALL 8
 BOTTOM WALL 16
+PORTAL 32
+TRAP 64
+WINGAME 128
 */
 
 var image = new Image(), mapImage = new Image(), tree = new Image(), monster = new Image(), portal = new Image(), wall = new Image();
@@ -16,31 +19,40 @@ wall.src = "img/wall.png";
 function map(loadmap) {
     this.cells = [];
     this.monsters = [];
+    this.blockedMonster = [];
     
     //WATING MOVE = 2, MOVING MONSTER = 3, WAPPO_ANIMATING = 4, WON_GAME = 5
     this.GAMESTATE = 2;
     
-    this.start = function() {
-	for(i = 0; i < 49; i++) {
-	    this.cells[i] = loadmap[i];
-	}
-	this.wappoX = loadmap[49];
-	this.wappoY = loadmap[50];
+    this.start = function () {
+        for (i = 0; i < 49; i += 1) {
+            this.cells[i] = loadmap[i];
+        }
+        this.wappoX = loadmap[49];
+        this.wappoY = loadmap[50];
 
-	for(i = 51; i < loadmap.length; i++) {
-	    this.monsters.push(loadmap[i]);
-	}
+        for(i = 51; i < loadmap.length; i++) {
+            this.monsters.push(loadmap[i]);
+            if (i%2 == 1) this.blockedMonster.push(0);
+        }
     };
     
     this.isValidPosition = function(x,y) {
-	if (x < 0 || x > 6 || y < 0 || y > 6) return false;
-	if (this.cells[7*x + y] & 1) return false;
-	return true;
+        if (x < 0 || x > 6 || y < 0 || y > 6) return false;
+        if (this.cells[7*x + y] & 1) return false;
+        return true;
     };
+    this.distance = function(x1,y1,x2,y2) {
+        console.log(x1 + "-" + y1 + "-" + x2 + "-" + y2);
+        return Math.abs(y2-y1) + Math.abs(x2-x1);
+    }
     this.canGoTo = function(to) {
-	i = this.wappoX, j = this.wappoY, p = this.cells[7*j + i];
-	return !((to == 0 && p & 8) || (to == 1 && p & 4) || (to == 2 && p & 16) || (to == 3 && p & 2));
-	
+        i = this.wappoX, j = this.wappoY, p = this.cells[7*j + i];
+        return !((to == 0 && p & 8) || (to == 1 && p & 4) || (to == 2 && p & 16) || (to == 3 && p & 2));
+    };
+    this.monsterCanGoTo = function(i,j,to) {
+        p = this.cells[7*j + i];
+        return !((to == 0 && p & 8) || (to == 1 && p & 4) || (to == 2 && p & 16) || (to == 3 && p & 2));
     };
     
     //back = 0, right = 1, front = 2, left = 3
@@ -49,132 +61,183 @@ function map(loadmap) {
     this.lastMove = 2;
     this.animate = 0;
     
+    this.monsterAnimate = 0;
+    
     //up = 0, right = 1, down = 2, left = 3
     this.move = function(to) {
-	if ((this.canGoTo(to)) && ((to == 0 && this.isValidPosition(this.wappoY-1,this.wappoX)) || (to == 1 && this.isValidPosition(this.wappoY,this.wappoX+1)) || (to == 2 && this.isValidPosition(this.wappoY+1,this.wappoX)) || (to == 3 && this.isValidPosition(this.wappoY,this.wappoX-1)))) {
-	    this.lastMove = to;
-	    this.GAMESTATE = 4;
-	}
+        if ((this.canGoTo(to)) && ((to == 0 && this.isValidPosition(this.wappoY-1,this.wappoX)) || (to == 1 && this.isValidPosition(this.wappoY,this.wappoX+1)) || (to == 2 && this.isValidPosition(this.wappoY+1,this.wappoX)) || (to == 3 && this.isValidPosition(this.wappoY,this.wappoX-1)))) {
+            for(i = 0; i < this.blockedMonster.length; i++) {
+                if(this.blockedMonster[i] > 0) this.blockedMonster[i]--;
+            }
+            this.lastMove = to;
+            this.GAMESTATE = 4;
+        }
     };
     
     this.clear = function() {
-	for(i = 0; i < 49; i++) {
-	    this.cells[i] = 0;
-	}
+        for(i = 0; i < 49; i++) {
+            this.cells[i] = 0;
+        }
     };
     
     this.drawWappo = function(ctx) {
-	var k = Math.floor(((myGameArea.frameNo)%8) /2);
-	if (this.GAMESTATE == 2) {
-	    ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX,52*this.wappoY,52,52);
-	}
-	else if (this.GAMESTATE == 4) {
-	    var to = this.lastMove;
-	    this.animate++;
-	    if (to == 0) {//UP, decrease Y
-		ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX,52*this.wappoY - 10*this.animate,52,52);
-	    }
-	    if (to == 1) {//RIGHT, increase X
-		ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX + this.animate*10,52*this.wappoY,52,52);
-	    }
-	    if (to == 2) {//DOWN, increase Y
-		ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX,52*this.wappoY + 10*this.animate,52,52);
-	    }
-	    if (to == 3) {//LEFT, decrease X
-		ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX - this.animate*10,52*this.wappoY,52,52);
-	    }
-	    if (this.animate == 5) {//End of animation. Go back to WAITING STATE and update the position of WAPPO
-		this.animate = 0;
-		this.GAMESTATE = 2;
-		if (to == 0 && this.isValidPosition(this.wappoY-1,this.wappoX)) {
-		    this.wappoY--;
-		}
-		else if (to == 1 && this.isValidPosition(this.wappoY,this.wappoX+1)) {
-		    this.wappoX++;
-		}
-		else if (to == 2 && this.isValidPosition(this.wappoY+1,this.wappoX)) {
-		    this.wappoY++;
-		}
-		else if (to == 3 && this.isValidPosition(this.wappoY,this.wappoX-1)) {
-		    this.wappoX--;
-		}
-		
-		k = this.cells[7*this.wappoY + this.wappoX];
-		joinedPortal = false;
-		if (k & 32) {
-		    for(i = 0; i < 7 && !joinedPortal; i++) {
-			for(j = 0; j < 7 && !joinedPortal; j++) {
-			    if (this.cells[7*j+i] & 32 && 7*j+i != 7*this.wappoY + this.wappoX) {
-				this.wappoY = j;
-				this.wappoX = i;
-				joinedPortal = true;
-			    }
-			}
-		    }
-		}
-		
-		if (k & 128) {
-		    this.GAMESTATE = 5;
-		}
-	    }
-	}
+        var k = Math.floor(((myGameArea.frameNo)%8) /2);
+        if (this.GAMESTATE == 2 || this.GAMESTATE == 3) {
+            ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX,52*this.wappoY,52,52);
+        }
+        else if (this.GAMESTATE == 4) {
+            var to = this.lastMove;
+            this.animate++;
+            if (to == 0) {//UP, decrease Y
+              ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX,52*this.wappoY - 10*this.animate,52,52);
+            }
+            if (to == 1) {//RIGHT, increase X
+              ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX + this.animate*10,52*this.wappoY,52,52);
+            }
+            if (to == 2) {//DOWN, increase Y
+              ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX,52*this.wappoY + 10*this.animate,52,52);
+            }
+            if (to == 3) {//LEFT, decrease X
+              ctx.drawImage(image,k*100,this.side * 100,100,100,52*this.wappoX - this.animate*10,52*this.wappoY,52,52);
+            }
+            if (this.animate == 5) {//End of animation. Go to MONSTER MOVEMENT STATE and update the position of WAPPO
+                this.animate = 0;
+                if (to == 0 && this.isValidPosition(this.wappoY-1,this.wappoX)) {
+                    this.wappoY--;
+                }
+                else if (to == 1 && this.isValidPosition(this.wappoY,this.wappoX+1)) {
+                    this.wappoX++;
+                }
+                else if (to == 2 && this.isValidPosition(this.wappoY+1,this.wappoX)) {
+                    this.wappoY++;
+                }
+                else if (to == 3 && this.isValidPosition(this.wappoY,this.wappoX-1)) {
+                    this.wappoX--;
+                }
+
+                k = this.cells[7*this.wappoY + this.wappoX];
+                joinedPortal = false;
+                if (k & 32) {
+                    for(i = 0; i < 7 && !joinedPortal; i++) {
+                        for(j = 0; j < 7 && !joinedPortal; j++) {
+                            if (this.cells[7*j+i] & 32 && 7*j+i != 7*this.wappoY + this.wappoX) {
+                                this.wappoY = j;
+                                this.wappoX = i;
+                                joinedPortal = true;
+                            }
+                        }
+                    }
+                }
+
+                this.GAMESTATE = 3;
+
+
+                if (k & 128) { //Won game
+                    this.GAMESTATE = 5;
+                }
+            }
+        }
     };
     
     
 		//Draw 
     this.draw = function(ctx) {
-	if (this.GAMESTATE == 5) {
-	    ctx.font = "30px Arial";
-	    ctx.fillText("CONGRATULATIONS!",30,100);
-	    ctx.fillText("YOU WON!",100,150);
-	    return;
-	}
+        if (this.GAMESTATE == 5) {
+            ctx.font = "30px Arial";
+            ctx.fillText("CONGRATULATIONS!",30,100);
+            ctx.fillText("YOU WON!",100,150);
+            return;
+        }
 	//draw map
-	for(i = 0; i < 7; i++) {
-	    for(j = 0; j < 7; j++) {
-		k = this.cells[7*j + i];
-		if (k & 64) {
-		    ctx.drawImage(mapImage,51,51*3,52,52,52*i,52*j,52,52);
-		}
-		else if (k & 128) {
-		    ctx.drawImage(mapImage,51,51*2,52,52,52*i,52*j,52,52);
-		}
-		else {
-		    ctx.drawImage(mapImage,51,0,52,52,52*i,52*j,52,52);
-		}
-		//check top wall
-		if (k & 8) {
-		    ctx.drawImage(wall,15,0,52,14,52*i,52*j -3,52,6);
-		}
-		//check left wall
-		if (k & 2) {
-		    ctx.drawImage(wall,0,0,14,52,52*i -3,52*j,6,52);
-		}
-		if (k & 1) {
-		    ctx.drawImage(tree,0,0,52,52,52*i,52*j,52,52);
-		}
-		//draw portal
-		if (k & 32) {
-		    ctx.drawImage(portal,(myGameArea.frameNo%4)*69,0,69,69,2+ 52*i,2 + 52*j,48,48);
-		}
-	    }
-	}
+        for(i = 0; i < 7; i++) {
+            for(j = 0; j < 7; j++) {
+                k = this.cells[7*j + i];
+                if (k & 64) {
+                    ctx.drawImage(mapImage,51,51*3,52,52,52*i,52*j,52,52);
+                }
+                else if (k & 128) {
+                    ctx.drawImage(mapImage,51,51*2,52,52,52*i,52*j,52,52);
+                }
+                else {
+                    ctx.drawImage(mapImage,51,0,52,52,52*i,52*j,52,52);
+                }
+                //check top wall
+                if (k & 8) {
+                    ctx.drawImage(wall,15,0,52,14,52*i,52*j -3,52,6);
+                }
+                //check left wall
+                if (k & 2) {
+                    ctx.drawImage(wall,0,0,14,52,52*i -3,52*j,6,52);
+                }
+                if (k & 1) {
+                    ctx.drawImage(tree,0,0,52,52,52*i,52*j,52,52);
+                }
+                //draw portal
+                if (k & 32) {
+                    ctx.drawImage(portal,(myGameArea.frameNo%4)*69,0,69,69,2+ 52*i,2 + 52*j,48,48);
+                }
+            }
+        }
 	
-	//draw wappo
-	this.drawWappo(ctx);
-	
-	//draw monsters
-	for(i = 0; i < this.monsters.length; i += 2) {
-	    if (this.monsters[i] < 10) {
-		ctx.drawImage(monster,(myGameArea.frameNo%4)*27,0,27,48,12 + 52*(this.monsters[i]%10),2 + 52*(this.monsters[i+1]%10),27,48);
-	    }
-	    else if (this.monsters[i] < 20) {
-		ctx.drawImage(monster,(myGameArea.frameNo%4)*27,48,27,48,12 + 52*(this.monsters[i]%10),2 + 52*(this.monsters[i+1]%10),27,48);
-	    }
-	    else {
-		ctx.drawImage(monster,(myGameArea.frameNo%4)*27,96,27,48,12 + 52*(this.monsters[i]%10),2 + 52*(this.monsters[i+1]%10),27,48);
-	    }
-	}
+	   //draw wappo
+	   this.drawWappo(ctx);
+        
+	   if (this.GAMESTATE == 3) {
+           this.monsterAnimate++;
+           if (this.monsterAnimate == 1 || this.monsterAnimate == 7) {//two movements
+                //up = 0, right = 1, down = 2, left = 3
+                //moving monsters.
+               anyMove = false;
+                for(i = 0; i < this.monsters.length; i += 2) {
+                    if (this.blockedMonster[i/2] > 0) continue;
+                    moved = false;
+                    _x1 = this.wappoX; _y1 = this.wappoY;
+                    _x2 = this.monsters[i]; _y2 = this.monsters[i+1]; currentD = this.distance(_x1,_y1,_x2,_y2);
+                    //direita
+                    if (this.distance(_x1,_y1,_x2+1,_y2) < currentD && this.monsterCanGoTo(_x2,_y2,1)) {
+                        this.monsters[i] += 1;
+                        moved = true;
+                    }
+                    //esquerda
+                    else if (this.distance(_x1,_y1,_x2-1,_y2) < currentD && this.monsterCanGoTo(_x2,_y2,3)) {
+                        this.monsters[i] -= 1;
+                        moved = true;
+                    }
+                    //cima
+                    else if (this.distance(_x1,_y1,_x2,_y2-1) < currentD && this.monsterCanGoTo(_x2,_y2,0)) {
+                        this.monsters[i+1] -= 1;
+                        moved = true;
+                    }
+                    //baixo
+                    else if (this.distance(_x1,_y1,_x2,_y2+1) < currentD && this.monsterCanGoTo(_x2,_y2,2)) {
+                        this.monsters[i+1] += 1;
+                        moved = true;
+                    }
+                    if (moved) {
+                        anyMove = true;
+                        if (this.cells[7*this.monsters[i+1] + this.monsters[i]] & 64) {
+                            this.blockedMonster[i/2] = 4;
+                        }    
+                    }
+                }
+               if (!anyMove) { this.GAMESTATE=2; this.monsterAnimate = 0; }
+           }
+           if (this.monsterAnimate == 12) { this.GAMESTATE=2; this.monsterAnimate = 0; }
+       }
+       if (this.GAMESTATE != 3 || this.monsterAnimate == 5 || this.monsterAnimate == 6) {
+           //draw monsters
+           for(i = 0; i < this.monsters.length; i += 2) {
+               if (this.monsters[i] < 10) {
+                   ctx.drawImage(monster,(myGameArea.frameNo%4)*27,0,27,48,12 + 52*(this.monsters[i]%10),2 + 52*(this.monsters[i+1]%10),27,48);
+               }
+               else if (this.monsters[i] < 20) {
+                   ctx.drawImage(monster,(myGameArea.frameNo%4)*27,48,27,48,12 + 52*(this.monsters[i]%10),2 + 52*(this.monsters[i+1]%10),27,48);
+               }
+               else {
+                   ctx.drawImage(monster,(myGameArea.frameNo%4)*27,96,27,48,12 + 52*(this.monsters[i]%10),2 + 52*(this.monsters[i+1]%10),27,48);
+               }
+           }
+       }
     };
     
     this.checkStatus = function(ctx) {
@@ -189,6 +252,4 @@ function map(loadmap) {
     instance = this;
     if (loadmap) { this.start(); }
     else { this.clear(); }
-    
-    
 }
--- js/wappogame.js
+++ js/wappogame.js
@@ -10,25 +10,25 @@ WINGAME 128
 */
 
 var firstMap = [
-    0,0,0,0,0,64,0,
-    0,16,0,0,4,22,2,
-    0,8,0,4,6,10,0,
-    68,38,2,0,16,4,2,
-    16,16,0,0,8,16,0,
-    140,10,4,6,2,56,0,
-    0,0,0,0,0,8,0,
+    0, 0, 0, 0, 0, 64, 0, 
+    0, 16, 0, 0, 4, 22, 2, 
+    0, 8, 0, 4, 6, 10, 0, 
+    68, 38, 2, 0, 16, 4, 2, 
+    16, 16, 0, 0, 8, 16, 0, 
+    140, 10, 4, 6, 2, 56, 0, 
+    0, 0, 0, 0, 0, 8, 0, 
     //starting position:
-    5,6,
+    5, 6, 
     //monsters position
-    1,1,
-    1,6
+    1, 1, 
+    1, 6
     //or more...
 ];
 
 var myMap = new map(firstMap);
 
 
-window.addEventListener('keydown',this.check,false);
+window.addEventListener('keydown', this.check,false);
 
 function check(e) {
     if (myMap.GAMESTATE != 2) return;
@@ -38,25 +38,24 @@ function check(e) {
     
     //Left arrow
     if (code == 37) {
-	myMap.side = 3;
-	myMap.move(3);
+        myMap.side = 3;
+        myMap.move(3);
     }
     //Up arrow
     if (code == 38) {
-	myMap.side = 0;
-	myMap.move(0);
+        myMap.side = 0;
+        myMap.move(0);
     }
     //Right arrow
     if (code == 39) {
-	myMap.side = 1;
-	myMap.move(1);
+        myMap.side = 1;
+        myMap.move(1);
     }
     //Down arrow
     if (code == 40) {
-	myMap.side = 2;
-	myMap.move(2);
+        myMap.side = 2;
+        myMap.move(2);
     }
-    
     myMap.checkStatus();
 }
 
@@ -88,8 +87,6 @@ function randomIntFromInterval(min,max) {
 
 
 function updateGameArea() {
-        
-    
     myGameArea.clear();
     myGameArea.frameNo += 1;
     ctx = myGameArea.context;
